b1824de187332619e05ddb6d9e2c2ca4f1d8a532
ekta-kaushish <ektakaushish95@gmail.com>
2025-06-14 19:12:28 +0530

    user resigtration done

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,20 @@
 const dotenv = require('dotenv')
 dotenv.config()
+const userRoutes = require('./routes/user.routes');
 
 const express = require('express')
-const app = express();
 
 const cors = require('cors');
+
+const connectToDb = require('./db/db')
+const app = express();
 app.use(cors());
+
+connectToDb();
+
+
+app.use('/users', userRoutes);
+
 
 app.get('/' ,(req,res)=>{
     res.send('HEloo')
